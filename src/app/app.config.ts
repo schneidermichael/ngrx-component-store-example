025c32f74a23c024ee9ed9aaa9b0a26dcbf23066
schneidermichael <schneidermichael@outlook.com>
2024-05-14 12:35:36 +0200

    chore: udpated packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,7 @@ import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
-  ],
+  providers: [provideRouter(routes)],
 };
